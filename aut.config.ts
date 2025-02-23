81d3aeafbd8074d8e0fe6c0c6802626f1128735f
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-02-23 16:10:35 +0200

    protected paths

--- aut.config.ts
+++ aut.config.ts
@@ -5,6 +5,24 @@ export const authConfig = {
   providers: [],
   callbacks: {
     authorized({ request, auth }: any) {
+      // Array of regex pattern of paths to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check of user is not authenticated and accessing the protected path
+      if (!auth && protectedPaths.some((path) => path.test(pathname)))
+        return false;
+
       // Check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie

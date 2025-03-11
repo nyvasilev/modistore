2f5004fcb2c9dfd70c323d491b331d3319cf860f
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-03-11 23:14:12 +0200

    order details table

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { notFound } from "next/navigation";
+import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 
 export const metadata: Metadata = {
@@ -13,7 +14,14 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   const order = await getOrderById(id);
 
   if (!order) notFound();
-  return <>Details</>;
+  return (
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+    />
+  );
 };
 
 export default OrderDetailsPage;

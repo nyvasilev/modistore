0a380ecb4bb35576c8803b3cbae95f11a1474a46
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-04-05 00:11:26 +0300

    update user form

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -2,6 +2,7 @@ import { requireAdmin } from "@/lib/auth-guard";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getUserById } from "@/lib/actions/user.actions";
+import UpdateUserForm from "./update-user-form";
 
 export const metadata: Metadata = {
   title: "Update User",
@@ -16,7 +17,12 @@ const AdminUserUpdateaPage = async (props: {
 
   if (!user) notFound();
 
-  return <div>Update</div>;
+  return (
+    <div className="space-y-8 max-w-lg mx-auto">
+      <h1 className="h2-bold">Update User</h1>
+      <UpdateUserForm user={user} />
+    </div>
+  );
 };
 
 export default AdminUserUpdateaPage;

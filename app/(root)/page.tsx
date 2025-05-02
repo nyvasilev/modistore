a7c38b11ad79d278e02c2cd0d9fe1706d083a90b
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-02 23:27:55 +0300

    icon boxes component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/lib/actions/products.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
+import IconBoxes from "@/components/icon-boxes";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -17,6 +18,7 @@ const Homepage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   );
 };

a1c81ad650facdfbafe60677f5dc392d975b2412
Nikolay Vasilev <nyvasilev11@gmail.com>
2025-05-03 17:30:39 +0300

    deal countdown component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,7 @@ import {
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
 import IconBoxes from "@/components/icon-boxes";
+import DealCountdown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -18,6 +19,7 @@ const Homepage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
+      <DealCountdown />
       <IconBoxes />
     </>
   );
